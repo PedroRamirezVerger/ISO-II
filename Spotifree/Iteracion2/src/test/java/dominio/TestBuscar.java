23b8407965f2bdce456c91aee13a84e169786cb9
Thor-Erik Moyano Díaz <thorerik.moyano@alu.uclm.es>
2017-12-20 20:22:35 +0100

    más TestBuscar

--- Spotifree/Iteracion2/src/test/java/dominio/TestBuscar.java
+++ Spotifree/Iteracion2/src/test/java/dominio/TestBuscar.java
@@ -31,11 +31,59 @@ public class TestBuscar {
 	public void testBucarCancion1() {
 		Cancion buscada = new Cancion(0, 0, null, null, null, null, 0);
 		buscada.setNombre("ejemplo");
-		ArrayList<Cancion> array = new ArrayList<Cancion>();
-		array.add(buscada);
-		Cancion res = buscar.buscarcancion(array, new String("ejemplo"));
+		ArrayList<Cancion> lista = new ArrayList<Cancion>();
+		lista.add(buscada);
+		Cancion res = buscar.buscarcancion(lista, new String("ejemplo"));
 		assertSame(res, buscada); // exactamente la misma referencia
 	}
-	
+
+	@Test
+	public void testBucarCancionPrimera() {
+		Cancion buscada = new Cancion(122, 123, "buscame", "pop", null, null, 12.95);
+		Cancion otra = new Cancion(220, 234, "buscameno", "pop", null, null, 0.25);
+		ArrayList<Cancion> lista = new ArrayList<Cancion>();
+		lista.add(buscada);
+		lista.add(otra);
+		Cancion res = buscar.buscarcancion(lista, new String("buscame"));
+		assertSame(res, buscada); // exactamente la misma referencia
+	}
+
+	@Test
+	public void testBucarCancionUltima() {
+		Cancion buscada = new Cancion(122, 123, "buscame", "pop", null, null, 12.95);
+		Cancion otra = new Cancion(220, 234, "buscameno", "pop", null, null, 0.25);
+		ArrayList<Cancion> lista = new ArrayList<Cancion>();
+		lista.add(otra);
+		lista.add(buscada);
+		Cancion res = buscar.buscarcancion(lista, new String("buscame"));
+		assertSame(res, buscada); // exactamente la misma referencia
+	}
+
+	@Test
+	public void testBucarCancionMedio() {
+		Cancion buscada = new Cancion(122, 123, "buscame", "pop", null, null, 12.95);
+		Cancion otra = new Cancion(220, 234, "buscameno", "pop", null, null, 0.25);
+		Cancion otramas = new Cancion(200, 234, "Je Nach", "other", null, null, 0.1);
+		ArrayList<Cancion> lista = new ArrayList<Cancion>();
+		lista.add(otra);
+		lista.add(buscada);
+		lista.add(otramas);
+		Cancion res = buscar.buscarcancion(lista, new String("buscame"));
+		assertSame(res, buscada); // exactamente la misma referencia
+	}
+
+	@Test
+	public void testBucarCancionFallo() {
+		Cancion buscada = new Cancion(500, 123, "Roundaout", "rock", null, null, 12.95);
+		Cancion otra = new Cancion(220, 234, "buscameno", "pop", null, null, 0.25);
+		Cancion otramas = new Cancion(200, 234, "Je Nach", "other", null, null, 0.1);
+		ArrayList<Cancion> lista = new ArrayList<Cancion>();
+		lista.add(otra);
+		lista.add(buscada);
+		lista.add(otramas);
+		Cancion res = buscar.buscarcancion(lista, new String("buscame"));
+		assertSame(res, null); // exactamente la misma referencia
+	}
+
 
 }
